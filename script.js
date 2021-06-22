d73a9b8db1303f2f83b7ea249875dbc681e25c91
S07K <shubham3431kumar@gmail.com>
2021-06-22 18:58:45 +0530

    added vision, mission

--- script.js
+++ script.js
@@ -42,3 +42,48 @@ window.addEventListener("scroll", function() {
         nav.classList.remove("scrolled");
     }
     },false); 
+
+// Get the modal
+var modal1 = document.getElementById("Modal1");
+var modal2 = document.getElementById("Modal2");
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("Btn1");
+var btn2 = document.getElementById("Btn2");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+var span2 = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+  modal1.style.display = "block";
+}
+btn2.onclick = function() {
+    modal2.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+  modal1.style.display = "none";
+}
+span2.onclick = function() {
+    modal2.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+if(modal1.style.display == "block"){
+    window.onclick = function(event) {
+        if (event.target == modal1) {
+          modal1.style.display = "none";
+        }
+      }
+}
+
+if(modal2.style.display == "block"){
+    window.onclick = function(event) {
+        if (event.target == modal2) {
+          modal2.style.display = "none";
+        }
+      }
+}
