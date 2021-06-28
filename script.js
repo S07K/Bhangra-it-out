90fc0d464a165a135f6fa7a01c2b11144cc13448
S07K <shubham3431kumar@gmail.com>
2021-06-28 20:55:21 +0530

    added dropdown for mobile view

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 if(window.innerWidth <= 1125 )
 {
     document.querySelector("#nav-right ul").classList.add("nav-links");
+    var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
 }
 
 const navslide = () => {
